eb1e7faab39fb7c472ed6faaee7e291e20db9ccf
unknown <a_olga_sergeevna@mail.ru>
2022-06-20 13:51:00 +0500

    task2

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,7 +15,14 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    let shippingSum;
 
+    if(productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+    if(productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -33,6 +40,12 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+    let discountSum;
+    if(productsSum >= discountMinSum) {
+        discountSum =  productsSum * discountPart / 100;
+    } else {
+        discountSum = 0;
+    }
 
     // Конец решения задания №2.2.
 
@@ -49,8 +62,16 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
+    let totalSum;
+
+    totalSum = productsSum - discountSum; 
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+    totalSum = totalSum + shippingSum;
+
+    let freeShipping;
+    freeShipping = !Boolean(shippingSum);
 
     // прибавьте к totalSum значение shippingSum
 
